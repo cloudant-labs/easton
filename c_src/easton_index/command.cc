9b8c25836f0652d0668e576a4099fbf74bdca068
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-07-12 18:00:34 -0500

    Implemented the query command
    This accepts a WKB and a few other parameters and returns a list of hits
    that meet the requested criteria. There last remaining major issue with
    this code is the necessisty to add a custom index vistor to
    easton::Index so that we're not loading all false negatives into RAM for
    each query.
    
    We'll also need to accept the other possible shape inputs instead of
    just the WKB as well as implement the CRS translations once the rest of
    that code exists.

--- c_src/easton_index/command.cc
+++ c_src/easton_index/command.cc
@@ -233,108 +233,82 @@ remove_entry(easton::Index::Ptr idx, io::Reader::Ptr reader)
 }
 
 
-// static void
-// do_query(easton_idx_t* idx, const uint8_t* cmd, uint32_t cmdlen)
-// {
-//     GEOSGeometry* query = NULL;
-//     easton_geom_filt_t* filtfun;
-//
-//     uint8_t* wkb;
-//     uint32_t wkblen;
-//     uint8_t filter;
-//     uint8_t nearest;
-//     uint64_t lim;
-//     uint64_t off;
-//     std::vector<bytes> docids;
-//     std::vector<bytes> wkbs;
-//     uint8_t* buf = NULL;
-//     uint8_t* pos = NULL;
-//     uint64_t buflen;
-//     uint64_t i;
-//
-//     if(!easton_read_binary(&cmd, &cmdlen, (const void**) &wkb, &wkblen)) {
-//         exit(EASTON_ERROR_BAD_WKB);
-//     }
-//
-//     if(!easton_read_uint8(&cmd, &cmdlen, &filter)) {
-//         exit(EASTON_ERROR_BAD_FILTER);
-//     }
-//
-//     if(!easton_read_uint8(&cmd, &cmdlen, &nearest)) {
-//         exit(EASTON_ERROR_BAD_NEAREST);
-//     }
-//
-//     if(!easton_read_uint64(&cmd, &cmdlen, &lim)) {
-//         exit(EASTON_ERROR_BAD_LIMIT);
-//     }
-//
-//     if(!easton_read_uint64(&cmd, &cmdlen, &off)) {
-//         exit(EASTON_ERROR_BAD_OFFSET);
-//     }
-//
-//     query = easton_geom_from_wkb(idx, wkb, wkblen);
-//     if(query == NULL) {
-//         easton_send_error(NULL, 0);
-//         goto done;
-//     }
-//
-//     filtfun = easton_geom_get_filter(idx, filter);
-//     if(filtfun == NULL) {
-//         easton_send_error(NULL, 0);
-//         goto done;
-//     }
-//
-//     if(!easton_index_query(idx, query, filt, nearest, lim, off, docids, wkbs)) {
-//         goto done;
-//     }
-//
-//     buflen = sizeof(uint64_t);
-//     for(i = 0; i < num_results; i++) {
-//         buflen += sizeof(uint32_t) + docidlens[i];
-//         buflen += sizeof(uint32_t) + wkblens[i];
-//     }
-//
-//     buf = (uint8_t*) malloc(buflen);
-//     pos = buf;
-//
-//     easton_write_uint64(pos, num_results);
-//     pos += sizeof(uint64_t);
-//
-//     for(i = 0; i < num_results; i++) {
-//         easton_write_binary(buf, &(docids[i][0]), docids[i].size());
-//         easton_write_binary(buf, &(wkbs[i][0]), wkbs[i].size());
-//     }
-//
-//     easton_send_ok(buf, buflen);
-//
-// done:
-//
-//     if(query != NULL) {
-//         GEOSGeom_destroy_r(idx->geos_ctx, query);
-//     }
-//
-//     if(docids != NULL) {
-//         free(docids);
-//     }
-//
-//     if(docidlens != NULL) {
-//         free(docidlens);
-//     }
-//
-//     if(wkbs != NULL) {
-//         free(wkbs);
-//     }
-//
-//     if(wkblens != NULL) {
-//         free(wkblens);
-//     }
-//
-//     if(buf != NULL) {
-//         free(buff);
-//     }
-//
-//     // Do *not* free the pos pointer.
-// }
+io::Writer::Ptr
+query(easton::Index::Ptr idx, io::Reader::Ptr reader)
+{
+    geo::Ctx::Ptr ctx = idx->get_geo_ctx();
+    io::Bytes::Ptr wkb = reader->read_bytes();
+    if(!wkb) {
+        throw EastonException("Invalid WKB argument for query.");
+    }
+
+    uint64_t filter;
+    bool nearest;
+    uint64_t limit;
+    uint64_t offset;
+
+    if(!reader->read(filter)) {
+        throw EastonException("Invalid filter argument for query.");
+    }
+
+    if(!reader->read(nearest)) {
+        throw EastonException("Invalid nearest argument for query.");
+    }
+
+    if(!reader->read(limit)) {
+        throw EastonException("Invalid limit argument for query.");
+    }
+
+    if(!reader->read(offset)) {
+        throw EastonException("Invalid offset argument for query.");
+    }
+
+    geo::Geom::Ptr query = ctx->from_wkb(wkb);
+    if(!query) {
+        throw EastonException("Unable to parse WKB geometry for query.");
+    }
+
+    geo::Bounds::Ptr bounds = query->get_bounds();
+    std::vector<easton::Index::Result> results = idx->query(bounds, nearest);
+
+    geo::GeomFilter filtfun = ctx->make_filter(query, filter);
+
+    // Discard skip results and only keep up to
+    // limit hits.
+    std::vector<easton::Index::Result> hits;
+    while(results.size() && hits.size() < limit) {
+        easton::Index::Result r = results.at(0);
+        results.erase(results.begin());
+
+        geo::Geom::Ptr hit = ctx->from_wkb(r.second);
+
+        // Anything that doesn't pass our filter was
+        // a false positive so ignore it.
+        if(!filtfun(hit)) {
+            continue;
+        }
+
+        // Skip past the requested number of hits
+        if(offset > 0) {
+            offset--;
+            continue;
+        }
+
+        hits.push_back(r);
+    }
+
+    io::Writer::Ptr writer = io::Writer::create();
+    writer->start_tuple(2);
+    writer->write("ok");
+    writer->start_list(hits.size());
+    for(uint32_t i = 0; i < hits.size(); i++) {
+        easton::Index::Result r = hits.at(i);
+        writer->start_tuple(2);
+        writer->write(r.first);
+        writer->write(r.second);
+    }
+    return writer;
+}
 
 
 io::Writer::Ptr
@@ -369,8 +343,8 @@ handle(easton::Index::Ptr idx, io::Reader::Ptr reader)
             return update_entry(idx, reader);
         case EASTON_COMMAND_REMOVE_ENTRIES:
             return remove_entry(idx, reader);
-        // case EASTON_COMMAND_QUERY:
-        //     return do_query(idx, reader);
+        case EASTON_COMMAND_QUERY:
+            return query(idx, reader);
         default:
             throw EastonException("Unknown command op.");
     }
