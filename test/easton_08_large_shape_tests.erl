887ba75fa1a8f8ecd5ed3132d16db7c0e7b3dac1
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-10-02 17:13:32 -0500

    Add a test for indexing and querying large shapes
    I failed to anticipate buffer sizes on the pipes that connect Erlang to
    easton_index. This test demonstrates the inability to read packets
    larger than 65K.

---
+++ test/easton_08_large_shape_tests.erl
@@ -0,0 +1,66 @@
+-module(easton_08_large_shape_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+idx_dir() ->
+    "idx/08".
+
+
+open_idx() ->
+    case filelib:is_dir(idx_dir()) of
+        true ->
+            ok = easton_index:destroy(idx_dir());
+        false ->
+            ok
+    end,
+    {ok, Idx} = easton_index:open(idx_dir()),
+    Idx.
+
+
+close_idx(Idx) ->
+    easton_index:close(Idx).
+
+
+large_shape_test() ->
+    Idx = open_idx(),
+    try
+        run(Idx)
+    after
+        close_idx(Idx)
+    end.
+
+run(Idx) ->
+    Shape = circle(),
+    QueryPoint = {[
+        {<<"type">>, <<"Point">>},
+        {<<"coordinates">>, [0.0, 0.0]}
+    ]},
+    ?assertEqual(ok, easton_index:update(Idx, <<"foo">>, Shape)),
+    ?assertEqual({ok, 1}, easton_index:doc_id_num(Idx)),
+    ?assertEqual({ok, 1}, easton_index:doc_count(Idx)),
+    ?assertEqual(
+        {ok, [{<<"foo">>, 0.0, Shape}]},
+        easton_index:search(Idx, Shape, [include_geom])
+    ),
+    ?assertMatch(
+        {ok, [{<<"foo">>, _, Shape}]},
+        easton_index:search(Idx, QueryPoint, [include_geom])
+    ),
+    ?assertEqual(ok, easton_index:remove(Idx, <<"foo">>)),
+    ?assertEqual({ok, 1}, easton_index:doc_id_num(Idx)),
+    ?assertEqual({ok, 0}, easton_index:doc_count(Idx)),
+    ?assertEqual({ok, []}, easton_index:search(Idx, Shape)).
+
+
+circle() ->
+    NumPoints = 10000,
+    Unit = 2.0 * math:pi() / NumPoints,
+    Coords0 = lists:map(fun(I) ->
+        [math:sin(I * Unit), math:cos(I * Unit)]
+    end, lists:seq(0, NumPoints-1)),
+    Coords = Coords0 ++ [hd(Coords0)],
+    {[
+        {<<"type">>, <<"Polygon">>},
+        {<<"coordinates">>, [Coords]}
+    ]}.
